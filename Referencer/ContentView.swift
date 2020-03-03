e65d13e3cb0fbd7b23c5d71d84567469c5625ce5
Sadeed Ahmad <sadeedahmadch@gmail.com>
2020-03-03 23:49:13 +1000

    adding image

--- Referencer/ContentView.swift
+++ Referencer/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+       
+        VStack() {
+            Image("gabba")
+                
+            Text("The Gabba Stadium")
+                .font(.largeTitle)
+        }
     }
 }
 

bee7ab513d4813b9563a2acf4e121e97802cf73e
Sadeed Ahmad <sadeedahmadch@gmail.com>
2020-03-04 00:58:54 +1000

    image done

--- Referencer/ContentView.swift
+++ Referencer/ContentView.swift
@@ -13,10 +13,33 @@ struct ContentView: View {
        
         VStack() {
             Image("gabba")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 400.0,height:400)
+                
                 
             Text("The Gabba Stadium")
                 .font(.largeTitle)
+                .bold()
+            
+            
+                Text("Capacity:")
+                    .bold()
+                
+                Text("City:")
+                    .bold()
+                Text("Sport:")
+                    .bold()
+            
+            
+            
+            
+            
+            
+                
+            
         }
+        
     }
 }
 
